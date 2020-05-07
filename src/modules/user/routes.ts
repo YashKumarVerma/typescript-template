850bbb1c53f305f63796aa2277cae0e4b1465c38
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-05-07 09:01:31 +0530

    Adding Eslint, prettierrc configs

--- src/modules/user/routes.ts
+++ src/modules/user/routes.ts
@@ -1,4 +1,4 @@
-mport express, { Response } from 'express'
+import express, { Response } from 'express'
 
 import UserOperations from './controller'
 
@@ -7,7 +7,9 @@ import { ControllerResponse } from './interface'
 const router = express.Router()
 
 router.post('/create', async ({ body }, res: Response) => {
-  const response = (await UserOperations.createNewUser(body)) as ControllerResponse
+  const response = (await UserOperations.createNewUser(
+    body,
+  )) as ControllerResponse
 
   res.status(response.code).json({
     error: response.error,
